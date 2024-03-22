4c654eb15ee704dcfb43c7035b06e30380b47da1
Vivek <vivekanandareddy7677@gmail.com>
2024-03-21 20:17:11 -0500

    Updating Mysql installation headings

--- mysql.sh
+++ mysql.sh
@@ -1,19 +1,19 @@
 source common.sh
-mysql_root_password = $1
+mysql_root_password=$1
 if [ -z "${mysql_root_password}" ]; then
   echo Input password is missing
   exit 1
 fi
 
-Print_Task_Heading "Installing Nginx"
+Print_Task_Heading "Installing Mysql Server"
 dnf install mysql-server -y &>>$LOG
 Check_Status $?
 
-Print_Task_Heading "Installing Nginx"
+Print_Task_Heading "Start MYSQL Service"
 systemctl enable mysqld &>>$LOG
 systemctl start mysqld &>>$LOG
 Check_Status $?
 
-Print_Task_Heading "Installing Nginx"
+Print_Task_Heading "Setup MYSQL Pass"
 mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$LOG
 Check_Status $?
